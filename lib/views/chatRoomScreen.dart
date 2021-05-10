edb99c879ad54361b8a28e3a9194e8c5fa958dcb
didierganthier <didierganthierperan@gmail.com>
2021-05-10 14:46:56 -0400

    sign out added in chatRoomScreen.dart

--- lib/views/chatRoomScreen.dart
+++ lib/views/chatRoomScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/services/auth.dart';
 
 class ChatRoomScreen extends StatefulWidget {
   @override
@@ -6,8 +7,26 @@ class ChatRoomScreen extends StatefulWidget {
 }
 
 class _ChatRoomScreenState extends State<ChatRoomScreen> {
+
+  AuthMethods authMethods = AuthMethods();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Logo App"),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              authMethods.signOut();
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(Icons.exit_to_app),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
