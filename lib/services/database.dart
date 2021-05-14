1d13343f8eb4feaefae366c3076aef34df5af513
didierganthier <didierganthierperan@gmail.com>
2021-05-14 15:16:21 -0400

    getUserByUsername created in database.dart

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,9 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseMethods {
-  getUserByUsername(String username) {}
+  getUserByUsername(String username) {
+    FirebaseFirestore.instance
+        .collection("users")
+        .where("name", isEqualTo: username)
+        .get();
+  }
 
-  uploadUserInfo(Map userMap) {
+   uploadUserInfo(Map userMap) {
     FirebaseFirestore.instance.collection("users").add(userMap).catchError((e) {
       print(e.toString());
     });
